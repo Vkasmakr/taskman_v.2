f5a7a243b1a9d314e4e6da66cc84b588aaeb273f
Vytautas K <makrelion@gmail.com>
2023-01-31 15:12:31 +0200

    Included comments to signals.py and urls.py pages

--- mysite/taskman/urls.py
+++ mysite/taskman/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    # general links to top pages of base.html
     path('', views.index, name='index'),
     path('employee/', views.employee, name='employee-page'),
     path('myteam/', views.MyTeamListView.as_view(), name='my-team'),
 
+    # links to task management and view pages
     path('tasks/', views.TaskListView.as_view(), name='tasks_name'),
     path('tasks/<int:pk>', views.TaskDetailView.as_view(), name='task-detail'),
     path('alltasks/', views.AllTaskListView.as_view(), name='all_tasks'),
@@ -14,8 +16,14 @@ urlpatterns = [
     path('alltasks/<int:pk>/open', views.task_open, name='open-task'),
     path('alltasks/<int:pk>/delete', views.TaskInstanceDeleteView.as_view(), name='delete-task'),
 
+    path('alltasks/new', views.TaskInstanceCreateView.as_view(), name='task-new'),
+    path('projects/<int:pk>/update', views.TaskInstanceUpdateView.as_view(), name='task-update'),
+    path('projects/<int:pk>/delete', views.TaskInstanceDeleteView.as_view(), name='task-delete'),
+
+    # general link to login/logout pages
     path('accounts/', include('django.contrib.auth.urls')),
 
+    # links to project management and view pages
     path('projects/', views.ProjectListView.as_view(), name='all_projects'),
     path('projectsc/', views.ProjectCListView.as_view(), name='all_projects_c'),
     path('projects/<int:pk>', views.ProjectDetailView.as_view(), name='project-detail'),
@@ -25,8 +33,4 @@ urlpatterns = [
     path('projects/new', views.ProjectCreateView.as_view(), name='project-new'),
     path('projects/<int:pk>/update', views.ProjectUpdateView.as_view(), name='project-update'),
     path('projects/<int:pk>/delete', views.ProjectDeleteView.as_view(), name='project-delete'),
-
-    path('alltasks/new', views.TaskInstanceCreateView.as_view(), name='task-new'),
-    path('projects/<int:pk>/update', views.TaskInstanceUpdateView.as_view(), name='task-update'),
-    path('projects/<int:pk>/delete', views.TaskInstanceDeleteView.as_view(), name='task-delete')
 ]
