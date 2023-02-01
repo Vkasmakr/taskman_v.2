d65d5745aedff0a1370003c20587d92b3fa2e58a
Vytautas K <makrelion@gmail.com>
2023-02-01 10:31:31 +0200

    fixed a bug where server did not load profile pictures

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('taskman/', include('taskman.urls')),
     path('admin/', admin.site.urls),
     path("", RedirectView.as_view(url="taskman/", permanent=True))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+              static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
